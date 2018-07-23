2a550582ee60a199dd5b6b7e0d23828a3f680b84
Vishwa97 <41324896+Vishwa97@users.noreply.github.com>
2018-07-23 18:23:11 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Sum
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int totaL=0;
+        for(int i=1;i<=n;i++)
+        {
+        totaL+=i;
+        }
+        {
+            System.out.println(totaL);
+        }
+        
+    }
+}
+        
