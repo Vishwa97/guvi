eccbb832eaca1078447839693a2cc6ce06723d07
Vishwa97 <41324896+Vishwa97@users.noreply.github.com>
2018-08-04 08:38:32 -0700

    Create InterOdd.java

---
+++ InterOdd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class InterOdd
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        for(int i=n;i<k;i++)
+        {
+            if(i%2!=0)
+            {
+            System.out.println(i);
+            }
+        }
+    }
+}
