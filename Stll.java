0c1bc05b20f24c8776002b187f78a8c0ad87ed67
Vishwa97 <41324896+Vishwa97@users.noreply.github.com>
2018-08-16 17:01:43 +0530

    Create Stll.java

---
+++ Stll.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Stll
+    {
+        public static void main(String arg[])
+        {
+            Scanner sc=new Scanner(System.in);
+            String s=sc.nextLine();
+            System.out.print(s+".");
+        }
+    }
